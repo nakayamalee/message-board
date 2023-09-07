59fcd984717bca0dd6f0142476239eaa59087413
tzuchi07 <140991232+tzuchi07@users.noreply.github.com>
2023-09-07 15:16:18 +0800

    modify2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CartTypeController;
 use App\Http\Controllers\HwController;
 use App\Http\Controllers\Controller;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +19,21 @@ use App\Http\Controllers\Controller;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 // Route::get('/', [FrontController::class, ''])->name('');
 
@@ -39,5 +53,3 @@ Route::resource('/hw', HwController::class);
 Route::post('/hw/re/{id}',[HwController::class, ('hwRe')])->name('nameRe');
 Route::put('/hw/reModify/{id}',[HwController::class, ('hwReModify')])->name('nameReModify');
 Route::delete('/hw/reDelete/{id}',[HwController::class, ('hwReDelete')])->name('nameDelete');
-
-
